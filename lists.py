be47e0b672cd6b3b8929495d3eb73ec0a0d0c82f
Ramesh <nagurramesh221b@gmail.com>
2019-08-27 14:06:15 +0530

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,13 @@
+
+
+
+value='sherlock'
+
+new_list=[54,5,34,4,24]
+#print(new_list.pop(0))
+#print(new_list)
+strings='I am studying in paaavai'
+print(strings)
+print(strings.count('a'))
+#print(strings.index('a',3))
+my_new_list=strings.split()
---
+++ sets.py
@@ -0,0 +1,5 @@
+mylist=[4,6,4,6,4,4,67,6]
+print(set(mylist))
+my_dict= { 'siva':[45,6,4,6,4,5], 'sanjay':{'c':[45,3,67,34,6],'java':[45,4,3,6,8]}}      
+print(my_dict)
+print(my_dict.keys())
